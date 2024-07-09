37272c80c593f0a24469a68be072cbc267a8897b
Marko Lumbar <markolumbar@Markos-MacBook-Pro.local>
2024-07-09 11:35:16 +0200

    konec prvega dela

--- src/main/java/com/example/demo/notes/NoteController.java
+++ src/main/java/com/example/demo/notes/NoteController.java
@@ -12,12 +12,14 @@ public class NoteController {
         this.noteRepository = noteRepository;
     }
 
+    // tukaj dobimo note oz sporocilo
     @GetMapping("/note/{id}")
     public String getNote(@PathVariable Long id) {
         return noteRepository.findById(id).get().getText();
 
     }
 
+    // tukaj ustvarjamo note oz sporocilo
     @PostMapping("/note")
     public NoteEntity createNote(@RequestBody CreateNote createNote){
         NoteEntity note = new NoteEntity();

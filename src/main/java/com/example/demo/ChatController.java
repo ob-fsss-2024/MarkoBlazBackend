9458f687359887bccb0268ca9b9a4624c46c4b13
Marko Lumbar <markolumbar@Markos-MacBook-Pro.local>
2024-07-10 10:10:48 +0200

    AI added

---
+++ src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final AzureOpenAiChatModel chatModel;
+
+    @Autowired
+    public ChatController(AzureOpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a fun fact about coctail ") String message) {
+        return Map.of("generation", chatModel.call(message));
+    }
+
+}
